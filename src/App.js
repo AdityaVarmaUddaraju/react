3e3d6911d96e343a94596257163b5504bdd268e7
Aditya varma <adithya951@gmail.com>
2020-01-20 11:28:28 +0530

    handling events with methods

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person'
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+state = {
+  person: [
+    {
+      name: 'Aditya',
+      age: 21
+    },
+    {
+      name: 'Howard Roark',
+      age: 0
+    }
+  ]
+}
+
+onChangeHandler = (event) => {
+  this.setState({
+    person: [
+      {
+        name: event.target.value,
+        age: 21
+      },
+      {
+        name: 'Howard Roark',
+        age: 0
+      }
+    ]
+  })
+}
+
+  render() {
   return (
     <div className="App">
       <h1>Welcome to react</h1>
-      <Person name='Aditya' age={21}/>
-      <Person name='Howard Roark' age={0}>My Hobbies: Designing</Person>
+      <button onClick={this.switchNameHandler}>Change name</button>
+      <Person onChange={this.onChangeHandler} name={this.state.person[0].name} age={this.state.person[0].age}/>
+      <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Designing</Person>
     </div>
   );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,9 @@ import React from 'react';
 const person = (props) => {
     return(
         <div>
-<p>My name is{props.name} and my age is {props.age}</p>
+<p>My name is {props.name} and my age is {props.age}</p>
     <p>{props.children}</p>
+    <input type="text" value={props.name} onChange={props.onChange} />
         </div>
     )
 }
